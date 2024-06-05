178219e314af940643cc9ac887e6a98590c75b5b
DuongHoang673 <hoangducduong03kjzg@gmail.com>
2024-06-05 21:11:26 +0700

    processing

--- app.js
+++ app.js
@@ -42,3 +42,18 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
--- middleware.js
+++ middleware.js
@@ -34,3 +34,18 @@ module.exports.isEmployeeOrAdmin = (req, res, next) => {
 };
 module.exports.verifyMemberId = async (req, res, next) => {
     const { memberId } = req.body;
+
+    // Check if the memberId is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(memberId)) {
+        req.flash('error', 'Invalid member ID format');
+        return res.redirect('/enroll');
+    }
+
+    const member = await Member.findById(memberId);
+    if (!member) {
+        req.flash('error', 'Invalid member ID');
+        return res.redirect('/enroll');
+    }
+    req.member = member;
+    next();
+};
